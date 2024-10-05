30a327f5a110ef976d87e155ea9cb370d96bd8ad
Shaswata Roy <shaswataroy1234@gmail.com>
2024-10-05 22:17:51 +0530

    Edit in ui

--- Gemini-clone/src/components/main/Main.jsx
+++ Gemini-clone/src/components/main/Main.jsx
@@ -31,24 +31,6 @@ const Main = () => {
               </p>
               <p className="t2">How can i help you today,</p>
             </div>
-            <div className="cards">
-              <div className="card">
-                <p>Suggest beautiful places to see on an upcoming road trip</p>
-                <img src={assets.compass_icon} />
-              </div>
-              <div className="card">
-                <p>Briefly summarize this concept : urabn planning</p>
-                <img src={assets.bulb_icon} />
-              </div>
-              <div className="card">
-                <p>Brainsrom team bonding activites for our work retreat</p>
-                <img src={assets.message_icon} />
-              </div>
-              <div className="card">
-                <p>Improve the readabilty of he follwing code</p>
-                <img src={assets.code_icon} />
-              </div>
-            </div>
           </>
         ) : (
           <div className="result">
